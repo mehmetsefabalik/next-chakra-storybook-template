9aec34c564ea03c87e396046e7c82c2dbbe3cc2f
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2022-01-16 11:27:56 +0300

    project is initiated, libraries are added, font is added

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    'storybook-css-modules-preset'
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: `"TTNorms", "Roboto", "Helvetica", "Arial", sans-serif`,
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return <ThemeProvider theme={theme}>
+    <Head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <Component {...pageProps} />;
+  </ThemeProvider>;
 }
 
-export default MyApp
+export default App;
